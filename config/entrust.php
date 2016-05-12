abb05cca6d6baacefab2b9a33c0dc37b47cc1ea5
Daniel Elliott <drelliott88@gmail.com>
2016-05-13 09:45:21 +1000

    Initial commit

--- config/entrust.php
+++ config/entrust.php
@@ -19,7 +19,7 @@ return [
     | the role if it is in a different namespace.
     |
     */
-    'role' => 'app\Models\Role',
+    'role' => 'rvs\Models\Role',
 
     /*
     |--------------------------------------------------------------------------
@@ -40,7 +40,7 @@ return [
     | Update the permission if it is in a different namespace.
     |
     */
-    'permission' => 'app\Models\Permission',
+    'permission' => 'rvs\Models\Permission',
 
     /*
     |--------------------------------------------------------------------------
--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => app\Models\User::class,
+        'model' => app\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
